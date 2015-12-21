696d52320c6be7e9cbca280d4343fedf16abd496
Mateusz Pokora <m.pokora@binarapps.com>
2015-12-21 15:42:12 +0100

    [add] snippet actions test

--- client/test/actions/snippet-actions.test.js
+++ client/test/actions/snippet-actions.test.js
@@ -3,7 +3,6 @@ import React from 'react';
 import alt from '../../js/libs/alt';
 import ReactDOM from 'react-dom';
 /*eslint-enable no-unused-vars*/
-// import TestUtils from 'react-addons-test-utils';
 import chai from 'chai';
 import sinon from 'sinon';
 
@@ -12,22 +11,62 @@ import SnippetActions from '../../js/actions/snippet-actions';
 const expect = chai.expect;
 
 describe('SnippetActions', () => {
-  it('should dispatch all snippets list from server', (done) => {
-    var snippets = [
-      { id: 1, content: 'test', name: 'test', description: 'test', language: 'javascript'},
-      { id: 2, content: 'test', name: 'test', description: 'test', language: 'javascript'}
-    ];
-
-    var server = sinon.fakeServer.create({
+  var snippets, server;
+  before(() => {
+    server = sinon.fakeServer.create({
       autoRespond: true,
       respondImmediately: true
     });
     sinon.spy(alt, 'dispatch');
-    server.respondWith('GET', '/snippets', [200, { 'Content-Type': 'application/json' }, JSON.stringify(snippets)]);
-    SnippetActions.getAll();
-    setTimeout(function () {
-      expect(alt.dispatch.calledOnce).to.be.true;
-      done();
+  });
+
+  afterEach(() => {
+    alt.dispatch.reset();
+  });
+
+  describe('Get snippets collection', () => {
+    before(() => {
+      snippets = [
+        { id: 1, content: 'test', name: 'test', description: 'test', language: 'javascript'},
+        { id: 2, content: 'test', name: 'test', description: 'test', language: 'javascript'}
+      ];
+    });
+
+    it('should dispatch all snippets list from server', (done) => {
+      server.respondWith('GET', '/snippets', [200, { 'Content-Type': 'application/json' }, JSON.stringify(snippets)]);
+      SnippetActions.getAll();
+      setTimeout(function () {
+        expect(alt.dispatch.calledOnce).to.be.true;
+        expect(alt.dispatch.getCall(0).args[1].snippets).to.deep.equal(snippets);
+        done();
+      });
+    });
+
+    it('should dispatch all snippets with name from server', (done) => {
+      server.respondWith('GET', '/snippets/search?name=test', [200, { 'Content-Type': 'application/json' }, JSON.stringify(snippets)]);
+      SnippetActions.search('test');
+      setTimeout(function () {
+        expect(alt.dispatch.calledOnce).to.be.true;
+        expect(alt.dispatch.getCall(0).args[1].snippets).to.deep.equal(snippets);
+        done();
+      });
     });
   });
+
+  describe('Create snippet', () => {
+    var snippet;
+    before( function() {
+      snippet = { id: 1, content: 'test', name: 'test', description: 'test', language: 'javascript'};
+    });
+    it('should dispatch created snippet', (done) => {
+      server.respondWith('POST', '/snippets', [201, { 'Content-Type': 'application/json' }, JSON.stringify(snippet)]);
+      SnippetActions.create(snippet);
+      setTimeout(function () {
+        expect(alt.dispatch.calledOnce).to.be.true;
+        expect(alt.dispatch.getCall(0).args[1].snippet).to.deep.equal(snippet);
+        done();
+      });
+    });
+  });
+
 });
