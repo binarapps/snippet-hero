b7f2a9e96a0fe1732222f072545fdc07eb186287
Mateusz Pokora <m.pokora@binarapps.com>
2016-01-02 22:23:23 +0100

    [add] server logging

--- server/app.js
+++ server/app.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var path = require('path');
 // var favicon = require('serve-favicon');
+var fs = require('fs');
+var FileStreamRotator = require('file-stream-rotator');
 var logger = require('morgan');
+var appLogger = require('./lib/logger');
+
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
@@ -17,7 +21,26 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+// create a write stream (in append mode)
+
+// setup the logger
+var logDirectory = __dirname + '/log';
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+var logStream = FileStreamRotator.getStream({
+  filename: logDirectory + '/server-%DATE%.log',
+  frequency: 'daily',
+  verbose: false,
+  date_format: 'YYYY-MM-DD'
+});
+app.use(logger('combined', {stream: logStream}));
+app.use(logger('dev', {
+  stream: {
+    write: function(str) {
+      appLogger.info(str);
+    }
+  }
+}));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
---
+++ server/lib/logger.js
@@ -0,0 +1,4 @@
+var log4js = require('log4js');
+var appLogger = log4js.getLogger('cheese');
+
+module.exports = appLogger;
