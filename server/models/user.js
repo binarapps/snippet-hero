3484ebb84c0d3bdafe77c7bcf9d6b4c9eac348f0
Kamil Piotrowski <daffy92@gmail.com>
2016-01-04 11:41:05 +0100

    lint

--- server/models/user.js
+++ server/models/user.js
@@ -29,7 +29,7 @@ module.exports = function(sequelize, DataTypes) {
           id: this.get('id'),
           email: this.get('email'),
           name: this.get('email')
-        }
+        };
         return json;
       }
     }
