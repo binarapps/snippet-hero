855edbfb14134d2629e7a019f2e3621fec44b458
Kamil Piotrowski <daffy92@gmail.com>
2016-01-04 09:18:32 +0100

    session fix

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var models = require('../models');
 var passport = require('passport');
 var bcrypt = require('bcrypt-nodejs');
+var randtoken = require('rand-token');
 var User = models.User;
 
 /* GET users listing. */
@@ -18,10 +19,21 @@ router.get('/', function(req, res) {
 
 router.post('/login',
   passport.authenticate('local'),
-  function(req, res) {
+  function(req, res, next) {
     res.send({ user: req.user });
   }
 );
+
+router.get('/current',
+  function(req, res, next) {
+    if(req.user) {
+      res.send({ user: req.user });
+    } else {
+      res.status(401).send('Not logggend in');
+    }
+  }
+);
+
 
 router.delete('/logout', function(req, res) {
   req.logout();
@@ -38,7 +50,9 @@ router.post('/register', function(req, res) {
     passwordSalt: salt
   };
   User.create(attributes).then(function(user) {
-    res.status(201).send({ user: user });
+    req.logIn(user, function(err) {
+      res.status(201).send({ user: user });
+    });
   }).catch(function(err) {
     res.status(422).send(err.message);
   });
