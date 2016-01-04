57e212e818a430b11c468bb72fd1ff55da6aa464
Kamil Piotrowski <daffy92@gmail.com>
2016-01-04 16:22:18 +0100

    lint

--- server/services/slack-integration.js
+++ server/services/slack-integration.js
@@ -1,37 +1,38 @@
 var request = require('request');
 var secrets = require('../config/secrets');
 var settings = require('../config/settings');
+var appLogger = require('../lib/logger');
 
 var SlackIntegration = {
   link: function(url, linkName) {
     var link = '<' + url;
     if(linkName) {
-      link += '|' + linkName
+      link += '|' + linkName;
     }
-    link += '>'
+    link += '>';
     return link;
   },
   notify: function(text) {
     this.send({ text: text });
   },
   send: function(data) {
-    if(!secrets.slackHookUrl) { throw new Error("No slackHookUrl was provided in 'config/secrets.json'."); }
+    if(!secrets.slackHookUrl) { throw new Error('No slackHookUrl was provided in \'config/secrets.json\'.'); }
     var payload = {
       channel: data.channel || settings.slack.defaultChannel,
       username: data.username || settings.slack.defaultUsername,
       text: data.text,
       icon_emoji: data.iconEmoji || settings.slack.defaultIconEmoji
-    }
+    };
     var options = {
       url: secrets.slackHookUrl,
       body: JSON.stringify(payload)
-    }
-    request.post(options, function(err, response, body) {
+    };
+    request.post(options, function(err) {
       if(err) {
-        console.log(err);
+        appLogger.debug(err);
       }
     });
   }
-}
+};
 
 module.exports = SlackIntegration;
