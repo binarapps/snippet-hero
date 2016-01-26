6667fa235bac0e8325e9d6683f2cf5088e12de0c
dwoznicka <woznicka.daria@gmail.com>
2016-01-26 12:04:02 +0100

    travis yml

--- server/test/comment.test.js
+++ server/test/comment.test.js
@@ -27,12 +27,13 @@ describe('Comments routes', function() {
     var user;
     before(function(done) {
       db.Comment.sync({ force : true }).then(function() {
-        db.Snippet.sync({ force : true }).then(function() {
-          db.User.sync({ force : true }).then(function() {
+        return db.Snippet.sync({ force : true }).then(function() {
+          return db.User.sync({ force : true }).then(function() {
             factory.create('snippet', function() {
               factory.create('user', function(err, newUser) {
                 user = newUser;
-                if(!err) done();
+                if(!err) return done();
+                return done(err);
               });
             });
           });
