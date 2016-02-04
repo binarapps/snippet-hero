8082698208d8c86cb922264f68bbedf03f43f052
dwoznicka <woznicka.daria@gmail.com>
2016-02-04 11:49:23 +0100

    deleted unused routes

--- server/routes/snippets.js
+++ server/routes/snippets.js
@@ -56,17 +56,6 @@ router.get('/:id', function (req, res) {
   });
 });
 
-/* GET snippet's ratings */
-router.get('/:id/ratings', function (req, res) {
-  models.Rating.findAll({ where : { SnippetId: req.params.id } }).then(function (ratings) {
-    var mappedRatings = ratings.map(function (rating) {
-      return rating.toJson();
-    });
-    
-    res.send(mappedRatings);
-  });
-});
-
 /* POST new snippet  */
 router.post('/', function (req, res) {
   var userId = req.user.get('id');
@@ -95,50 +84,6 @@ router.post('/', function (req, res) {
   });
 });
 
-/* GET snippet's rating from user*/
-router.get('/:snippet_id/users/:user_id', function (req, res) {
-  models.Rating.findAll({ where: { UserId: req.params.user_id, SnippetId: req.params.snippet_id } }).then(function (ratings){
-    var mappedRatings = ratings.map(function (rating) {
-      return rating.toJson();
-    });
-
-    res.send(mappedRatings);
-  });
-});
-
-/* GET current user's rating for snippet */
-router.get('/:snippet_id/user', function (req, res) {
-  var snippetId = req.params.snippet_id;
-  if(req.user){
-    var userId = req.user.get('id');
-    models.Rating.findOne({ where : { SnippetId: snippetId , UserId: userId } }).then( function (rating) {
-      var grade = 0;
-      if (rating) {
-        grade = rating.value;
-      } 
-      var hash = {user: userId, rate: grade, snippet: snippetId};
-      res.status(201).send(hash);
-    });
-  } else {
-    res.status(422).send({user: null, rate: 0, snippet: snippetId});
-  }
-});
-
-/* GET snippet's average rating */
-router.get('/:id/avg', function (req, res){
-  var snippetId = req.params.id;
-  var sumRtings = 0.0;
-  models.Rating.sum('value', { where : { SnippetId: snippetId } }).then(function (sum){
-    sumRtings = sum;
-    models.Rating.count({ where : { SnippetId: snippetId } }).then(function (c){
-      var average = 0;
-      if(c > 0) {
-        average = (sumRtings/c);
-      }
-      res.status(200).send({avg: average.toFixed(2), snippetId: snippetId});
-    });
-  });
-});
 
 /* DELETE snippet and it's ratings/versions/comments */
 router.delete('/:id', function (req, res){
