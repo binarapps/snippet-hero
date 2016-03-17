9f4e87637a483ef974664faf811d47468e2e55f8
dwoznicka <woznicka.daria@gmail.com>
2016-03-17 10:40:24 +0100

    snackbar fixed - TODO select field in snippet form

--- client/js/components/flash-messages.jsx
+++ client/js/components/flash-messages.jsx
@@ -5,9 +5,10 @@ import FlashMessagesStore from '../stores/flash-messages-store';
 export default class FlashMessages extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { lastFlashMessage: false };
+    this.state = { lastFlashMessage: false, open: false };
 
     this._onStoreChanged = this._onStoreChanged.bind(this);
+    this._handleRequestClose = this._handleRequestClose.bind(this);
 
     FlashMessagesStore.listen(this._onStoreChanged);
   }
@@ -21,14 +22,22 @@ export default class FlashMessages extends React.Component {
     if (flashMessages.length > 0) {
       this.setState({ lastFlashMessage: flashMessages[flashMessages.length - 1] });
     }
-    this.refs.snackbar.show();
+    this.setState({ open: true});
   }
+
+  _handleRequestClose() {
+    this.setState({
+      open: false
+    });
+  };
 
   render() {
     return (
       <div>
         <Snackbar message={this.state.lastFlashMessage.content || ''}
                   ref="snackbar"
+                  open={this.state.open}
+                  onRequestClose={this._handleRequestClose}
                   autoHideDuration={this.state.lastFlashMessage.duration || 3000}
                   />
       </div>
